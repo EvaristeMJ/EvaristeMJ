a68aeecf21d3684333448f1c4d477d927cb2ad78
EvaristeMJ <maxence.jauberty@telecom-paris.fr>
2024-08-10 10:38:25 +0200

    style upd

---
+++ script/header.js
@@ -0,0 +1,4 @@
+const siteurl = "https://evaristemj.github.io/EvaristeMJ/";
+const header = document.createElement("header");
+header.innerHTML = "<h1>Welcome to my website!</h1>";
+document.body.prepend(header);
